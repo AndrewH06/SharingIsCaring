cb9a5ba3b86b4d022f8de698198af804fab29bda
Andrew Hale <andrew.z.hale1@gmail.com>
2020-01-15 15:02:38 -0800

    1/15/2020

---
+++ UI-Portfolio/View Controls/GolfController.c
@@ -0,0 +1,9 @@
+//
+//  GolfController.c
+//  UI-Portfolio - Andrew Hale
+//
+//  Created by Hale, Andrew on 1/15/20.
+//  Copyright © 2020 Hale, Andrew. All rights reserved.
+//
+
+#include "GolfController.h"
---
+++ UI-Portfolio/View Controls/GolfController.h
@@ -0,0 +1,30 @@
+//
+//  GolfController.h
+//  UI-Portfolio - Andrew Hale
+//
+//  Created by Hale, Andrew on 1/15/20.
+//  Copyright © 2020 Hale, Andrew. All rights reserved.
+//
+
+#ifndef GolfController_h
+#define GolfController_h
+
+#include <stdio.h>
+#import <UIKit/UIKit.h>
+
+#define speedScale 0.20
+#define speedDamping 0.90  // friction rate
+#define stopSpeed 5.0 // These values simulate speed and friction
+
+@interface GolfController : UIViewController
+@property (strong, nonatomic) IBOutlet UIImageView *hole;
+@property (strong, nonatomic) IBOutlet UIImageView *ball; //I changed from an actual image because I wanted a more simplistic design
+@property (nonatomic) CGPoint firstPoint;
+@property (nonatomic) CGPoint lastPoint;
+@property (nonatomic) float ballVelocityX;
+@property (nonatomic) float ballVelocityY;
+@property (strong, nonatomic) NSTimer *gameTimer;
+
+@end
+
+#endif /* GolfController_h */
