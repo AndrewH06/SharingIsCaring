5b9b35c8a9ca7a9419208f6a75a454578c0ce145
Andrew Hale <andrew.z.hale1@gmail.com>
2020-02-10 14:32:13 -0800

    try

---
+++ UI-Portfolio/View Controls/CardsController.c
@@ -0,0 +1,9 @@
+//
+//  CardsController.c
+//  UI-Portfolio - Andrew Hale
+//
+//  Created by Hale, Andrew on 2/10/20.
+//  Copyright © 2020 Hale, Andrew. All rights reserved.
+//
+
+#include "CardsController.h"
---
+++ UI-Portfolio/View Controls/CardsController.h
@@ -0,0 +1,20 @@
+//
+//  CardsController.h
+//  UI-Portfolio - Andrew Hale
+//
+//  Created by Hale, Andrew on 2/10/20.
+//  Copyright © 2020 Hale, Andrew. All rights reserved.
+//
+
+#ifndef CardsController_h
+#define CardsController_h
+
+#include <stdio.h>
+
+@interface CardsController : UIViewController
+
+@property (strong, nonatomic) UIButton UIImageView *start;
+@property (strong, nonatomic) UIButton UIImageView *hit;
+@property (strong, nonatomic) UIButton UIImageView *stay;
+
+#endif /* CardsController_h */
